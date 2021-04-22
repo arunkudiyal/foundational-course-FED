5a0050e3cb45df656170bf0c450c4e92c958d7ee
Arun Kudiyal <arunkudiyal@icloud.com>
2021-04-22 18:46:17 +0530

    JavaScript

---
+++ JavaScript/dom.js
@@ -0,0 +1,13 @@
+
+// EXAMINE THE DOM
+
+console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.body);
+// console.log(document.head);
+
+
+console.log(document.all[10]);
---
+++ JavaScript/main.js
@@ -0,0 +1,56 @@
+// var, let, const
+
+// number, string, boolean, null, undefined | typeof null = object
+
+// let variableName = value
+
+// String methods - a.length, a.toUPperCase(), a.toLowerCase(), substr(start, end), split('basis)
+
+// Arrays - Collection of diffrenet data types 
+// Methods - push(), pop(), unshift(), Array.isArray(array), indexOf('oranges')
+
+// Objects - { key: value } | . operator
+
+// Arrayof Obects - [ {Object 1}, {Object 2}, {Object 3}, ... ]
+
+// LOOPS
+
+// Conditionals - if, else, else-if, switch
+
+// Voter System | if male - 21 years | female - 18 years
+
+// Switch
+
+// FUNCTIONS
+
+// function to add two numbers
+
+function greeting2(name) {
+    return `Goodbye ${name}`;
+}
+
+// function greeting(name) {
+//     ans = greeting2(name);
+//     return ans;
+// }
+
+// Calling the function
+// console.log(greeting('Sai'));
+
+
+// ES6 Arrrow Functions -> Short cut of writing functions
+
+// const variableName/ functionName = (parameter(s)) => { // Body // }
+
+// const add = (a, b) => {
+//     // Body of the function
+//     console.log(`The sum is ${a+b}`);
+// }
+
+// add(100, 200)
+
+// const greeting = (name) => {
+//     ans = greeting2(name);
+//     console.log(ans);
+// }
+// console.log(greeting('Sai'));
